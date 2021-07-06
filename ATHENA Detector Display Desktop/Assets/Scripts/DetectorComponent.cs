e663eecbf6299c15cb3851d80707cbc39af90167
SeanBP <sbpreins@gmail.com>
2021-07-06 10:08:25 -0500

    Changed detector component boarders

--- ATHENA Detector Display Desktop/Assets/Scripts/DetectorComponent.cs
+++ ATHENA Detector Display Desktop/Assets/Scripts/DetectorComponent.cs
@@ -10,37 +10,40 @@ public class DetectorComponent : MonoBehaviour
     public float innerR = 1f;
     public float outerR = 2f;
     public float length = 3f;
-    
+    public float rotate = 0f;
+    public float offset = 0f;
+    public float alpha = 0.3f;
+    public bool boarders = true;
     // Start is called before the first frame update
     void Start()
     {
         Vector3[] vertices = new Vector3[sides * 4];
         int[] triangles = new int[sides * 12 * 4];
-        GameObject[] lines = new GameObject[sides * 4];
+        GameObject[] lines = new GameObject[sides * 8];
 
         int index = 0;
         int lineIndex = 0;
         for (int i = 0; i < sides; i++)
         {
-            float angle = (360f / sides) * i;
+            float angle = (360f / sides) * i + rotate;
             double theta = Math.PI * angle / 180.0;
-            vertices[index] = new Vector3(outerR * (float)Math.Cos(theta), outerR * (float)Math.Sin(theta), 0f);
+            vertices[index] = new Vector3(outerR * (float)Math.Cos(theta), outerR * (float)Math.Sin(theta), (-length/2) + offset);
             index++;
-            vertices[index] = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), 0f);
+            vertices[index] = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), (-length / 2) + offset);
             index++;
-            vertices[index] = new Vector3(outerR * (float)Math.Cos(theta), outerR * (float)Math.Sin(theta), length);
+            vertices[index] = new Vector3(outerR * (float)Math.Cos(theta), outerR * (float)Math.Sin(theta), (length/2) + offset);
             index++;
-            vertices[index] = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), length);
+            vertices[index] = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), (length/2) + offset);
             index++;
 
 
-            float angle2 = (360f / sides) * (i + 1);
+            float angle2 = (360f / sides) * (i + 1) + rotate;
             double theta2 = Math.PI * angle2 / 180.0;
             Vector3 start;
             Vector3 end;
             LineRenderer lr = new LineRenderer();
             Material whiteDiffuseMat = new Material(Shader.Find("Unlit/Texture"));
-            for (float j = 0f; j <= length; j = j + length)
+            for (float j = (-length / 2) + offset; j <= length/2; j = j + length)
             {
                 start = new Vector3(outerR * (float)Math.Cos(theta), outerR * (float)Math.Sin(theta), j);
                 end = new Vector3(outerR * (float)Math.Cos(theta2), outerR * (float)Math.Sin(theta2), j);
@@ -49,27 +52,68 @@ public class DetectorComponent : MonoBehaviour
                 lines[lineIndex].AddComponent<LineRenderer>();
                 lr = lines[lineIndex].GetComponent<LineRenderer>();
                 lr.material = whiteDiffuseMat;
-                lr.SetColors(Color.red, Color.red);
-                lr.SetWidth(0.05f, 0.05f);
-                //lr.endtWidth(0.05f);
+                lr.SetWidth(0.03f, 0.03f);
                 lr.SetPosition(0, start);
                 lr.SetPosition(1, end);
                 lineIndex++;
+                if (innerR != 0)
+                {
+                    start = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), j);
+                    end = new Vector3(innerR * (float)Math.Cos(theta2), innerR * (float)Math.Sin(theta2), j);
+                    lines[lineIndex] = new GameObject();
+                    lines[lineIndex].transform.position = start;
+                    lines[lineIndex].AddComponent<LineRenderer>();
+                    lr = lines[lineIndex].GetComponent<LineRenderer>();
+                    lr.material = whiteDiffuseMat;
+                    lr.SetWidth(0.03f, 0.03f);
+                    lr.SetPosition(0, start);
+                    lr.SetPosition(1, end);
+                    lineIndex++;
+                    if (sides <= 50)
+                    {
+                        start = new Vector3(outerR * (float)Math.Cos(theta), outerR * (float)Math.Sin(theta), j);
+                        end = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), j);
+                        lines[lineIndex] = new GameObject();
+                        lines[lineIndex].transform.position = start;
+                        lines[lineIndex].AddComponent<LineRenderer>();
+                        lr = lines[lineIndex].GetComponent<LineRenderer>();
+                        lr.material = whiteDiffuseMat;
+                        lr.SetWidth(0.03f, 0.03f);
+                        lr.SetPosition(0, start);
+                        lr.SetPosition(1, end);
+                        lineIndex++;
+                    }
 
-                start = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), j);
-                end = new Vector3(innerR * (float)Math.Cos(theta2), innerR * (float)Math.Sin(theta2), j);
+                }
+            }
+            if (sides <= 50)
+            {
+                start = new Vector3(outerR * (float)Math.Cos(theta), outerR * (float)Math.Sin(theta), (-length / 2) + offset);
+                end = new Vector3(outerR * (float)Math.Cos(theta), outerR * (float)Math.Sin(theta), (length/2) + offset);
                 lines[lineIndex] = new GameObject();
                 lines[lineIndex].transform.position = start;
                 lines[lineIndex].AddComponent<LineRenderer>();
                 lr = lines[lineIndex].GetComponent<LineRenderer>();
                 lr.material = whiteDiffuseMat;
-                //lr.SetColors(Color.red, Color.red);
-                lr.SetWidth(0.05f, 0.05f);
+                lr.SetWidth(0.03f, 0.03f);
                 lr.SetPosition(0, start);
                 lr.SetPosition(1, end);
                 lineIndex++;
+                if(innerR != 0)
+                {
+                    start = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), (-length / 2) + offset);
+                    end = new Vector3(innerR * (float)Math.Cos(theta), innerR * (float)Math.Sin(theta), (length / 2) + offset);
+                    lines[lineIndex] = new GameObject();
+                    lines[lineIndex].transform.position = start;
+                    lines[lineIndex].AddComponent<LineRenderer>();
+                    lr = lines[lineIndex].GetComponent<LineRenderer>();
+                    lr.material = whiteDiffuseMat;
+                    lr.SetWidth(0.03f, 0.03f);
+                    lr.SetPosition(0, start);
+                    lr.SetPosition(1, end);
+                    lineIndex++;
+                }
             }
-
 
         }
         index = 0;
@@ -155,18 +199,22 @@ public class DetectorComponent : MonoBehaviour
         for (int i = 0; i < lines.Length; i++)
         {
             //lines[i].transform.SetParent(gameObject.transform);
-            lines[i].transform.parent = gameObject.transform;
+            if (lines[i] != null)
+            {
+                lines[i].transform.parent = gameObject.transform;
+            }
+            if (!boarders)
+            {
+                Destroy(lines[i]);
+            }
         }
 
         Material material = new Material(Shader.Find("Transparent/Diffuse"));
         Color color = new Color(0f, 0f, 1f);
-        color.a = 0.5f;
+        color.a = alpha;
         material.color = color;
         
         gameObject.GetComponent<MeshRenderer>().material = material;
-
-        //gameObject.transform.position = new Vector3(1, 1, 1);
-
     }
 
     // Update is called once per frame
