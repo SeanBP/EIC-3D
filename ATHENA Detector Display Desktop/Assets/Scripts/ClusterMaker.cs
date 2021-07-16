e8e8ce63278421784d0f36baf550156ea471451a
SeanBP <sbpreins@gmail.com>
2021-07-15 22:23:44 -0500

    Fixed barrel cluster orientation

--- ATHENA Detector Display Desktop/Assets/Scripts/ClusterMaker.cs
+++ ATHENA Detector Display Desktop/Assets/Scripts/ClusterMaker.cs
@@ -10,7 +10,7 @@ public class ClusterMaker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var filename = "Event11.txt";
+        var filename = "Event10.txt";
         var source = new StreamReader(Application.dataPath + "/Collision Data/" + filename);
         var fileContents = source.ReadToEnd();
         source.Close();
@@ -101,39 +101,22 @@ public class ClusterMaker : MonoBehaviour
                 hits[i - start].transform.localScale = new Vector3((length + 0.01f), 0.05f, 0.05f);
                 pivot.transform.localPosition = new Vector3(x, y, z);
                 hits[i - start].transform.localPosition = new Vector3(x, y, z);
-                float angle = (float)Math.Atan(y / x);
-                //hits[i - start].transform.RotateAround(pivot.transform.position, new Vector3(0, 0, 1), angle * (180 / (float)Math.PI));
-
-                /*int side = Convert.ToInt32(Math.Floor(((float)Math.Atan(y/x) / 360f)*12));
-                Debug.Log(side+" "+ Math.Atan(y / x));
-                float angle = (float)side * (350f / 12f);
-                hits[i - start].transform.localScale = new Vector3(1f * length + 0.01f, 0.05f, 0.05f);
-                hits[i - start].transform.Rotate(0, 0, angle, Space.Self);
-                hits[i - start].transform.localPosition = new Vector3(x + ((float)Math.Cos(angle)* (length + 0.01f) / 2), y + ((float)Math.Sin(angle) * (length + 0.01f) / 2), z);
-                */
-                //int side = Convert.ToInt32(Math.Floor(((float)Math.Atan(y / x) / 360f) * 12));
-                //Debug.Log(side + " " + Math.Atan(y / x));
-                
+                float angle = (float)Math.Atan(y / x);               
                 float xmag = 1;
                 if (x != 0) {
                     xmag = (x / (float)Math.Abs(x));
-                }
-                float angle2 = (angle * (180 / (float)Math.PI) + 360) % 360;
+                }               
+                float angle2 = (float)(180 / Math.PI) * (float)Math.Atan(y / x);
                 int side = Convert.ToInt32(Math.Round(angle2 / (360f/12f)));
+
                 float desiredAngle = side * (360f / 12f);
-                //hits[i - start].transform.position = new Vector3(x + xmag*((float)Math.Cos(angle) * (length + 0.01f) / 2), y + ((float)Math.Sin(angle) * (length + 0.01f) / 2), z);
-                //hits[i - start].transform.RotateAround(pivot.transform.position, new Vector3(0, 0, 1), 0f * (180 / (float)Math.PI));
-
-                //hits[i - start].transform.position = new Vector3(x + ((float)Math.Cos(Math.PI * desiredAngle / 180.0) * (length + 0.01f) / 2), y + ((float)Math.Sin(Math.PI * desiredAngle / 180.0) * (length + 0.01f) / 2), z);
-
-                //hits[i - start].transform.position = new Vector3(0, 0, 0);
-
-                //Debug.Log(x + " " + desiredAngle+" "+Math.Cos(Math.PI * desiredAngle / 180.0) +" "+(x + ((float)Math.Cos(Math.PI*desiredAngle/180.0))));
+              
                 hits[i - start].transform.RotateAround(pivot.transform.position, new Vector3(0, 0, 1), desiredAngle);
+            
                 float magnitude = (float)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
                 float newX = xmag*(magnitude + ((length + 0.01f) / 2f)) * (float)Math.Cos(Math.PI * desiredAngle / 180.0f);
                 float newY = (magnitude + ((length + 0.01f) / 2f)) * (float)Math.Sin(Math.PI * desiredAngle / 180.0f);
-                hits[i - start].transform.position = new Vector3(newX, newY, z);
+                hits[i - start].transform.position = new Vector3(newX, xmag*newY, z);
             }
         }
         
