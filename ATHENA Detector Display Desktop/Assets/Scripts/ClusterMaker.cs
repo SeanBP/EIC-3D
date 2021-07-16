b6e10756cf0a21255813e212258bfa582ba054dd
SeanBP <sbpreins@gmail.com>
2021-07-15 21:41:58 -0500

    Made the barrel clusters oriented the right way. Still need to test it some.

--- ATHENA Detector Display Desktop/Assets/Scripts/ClusterMaker.cs
+++ ATHENA Detector Display Desktop/Assets/Scripts/ClusterMaker.cs
@@ -10,7 +10,7 @@ public class ClusterMaker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var filename = "Event10.txt";
+        var filename = "Event11.txt";
         var source = new StreamReader(Application.dataPath + "/Collision Data/" + filename);
         var fileContents = source.ReadToEnd();
         source.Close();
@@ -55,20 +55,13 @@ public class ClusterMaker : MonoBehaviour
         {
             var coords = lines[i].Split(" "[0]);
             Color color = new Color(0,0,0);
-
-            
-
-
+        
             x = float.Parse(coords[2]);
-                
-              
+                             
             y = float.Parse(coords[3]);
-                
-                
+                              
             z = float.Parse(coords[4]);
-                
-                
-            
+                                          
             hits[i-start] = GameObject.CreatePrimitive(PrimitiveType.Cube);
             
             hits[i-start].transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
@@ -77,41 +70,70 @@ public class ClusterMaker : MonoBehaviour
             hits[i-start].tag = "Cluster";
             hits[i - start].layer = 3;
             float length = (energyList[i-start] - minE) / (maxE - minE);
-            //Material material = new Material(Shader.Find("Diffuse"));
-            //Material material = hits[i - start].GetComponent<MeshRenderer>().sharedMaterial;
+           
             var cubeRenderer = hits[i - start].GetComponent<Renderer>();
             if (string.Equals(coords[0], "Ecal"))
             {
                 cubeRenderer.material.SetColor("_Color", Color.green);
-                //color = new Color(0, 1, 0);
-                //material.color = color;
             }
             if (string.Equals(coords[0], "Hcal"))
             {
                 cubeRenderer.material.SetColor("_Color", Color.blue);
-                //color = new Color(0, 1, 0);
-                //material.color = color;
             }
-            
-            //material.renderQueue = 1001;
-            //hits[i-start].GetComponent<Renderer>().material = material;
-            hits[i-start].transform.localScale = new Vector3(0.05f, 0.05f, 1f * length + 0.01f);
 
 
             if (string.Equals(coords[1], "Endcap"))
             {
+                hits[i - start].transform.localScale = new Vector3(0.05f, 0.05f, 1f * length + 0.01f);
                 if (z < 0)
                 {
-                    hits[i - start].transform.position = new Vector3(x, y, z - length/2);
+                    hits[i - start].transform.position = new Vector3(x, y, z - (length+0.01f)/2);
                 }
                 else
                 {
-                    hits[i - start].transform.position = new Vector3(x, y, z + length / 2);
+                    hits[i - start].transform.position = new Vector3(x, y, z + (length + 0.01f) / 2);
                 }
             }
             else
             {
-                hits[i - start].transform.position = new Vector3(x, y, z);
+                int sides = 12;
+                GameObject pivot = new GameObject();
+                hits[i - start].transform.localScale = new Vector3((length + 0.01f), 0.05f, 0.05f);
+                pivot.transform.localPosition = new Vector3(x, y, z);
+                hits[i - start].transform.localPosition = new Vector3(x, y, z);
+                float angle = (float)Math.Atan(y / x);
+                //hits[i - start].transform.RotateAround(pivot.transform.position, new Vector3(0, 0, 1), angle * (180 / (float)Math.PI));
+
+                /*int side = Convert.ToInt32(Math.Floor(((float)Math.Atan(y/x) / 360f)*12));
+                Debug.Log(side+" "+ Math.Atan(y / x));
+                float angle = (float)side * (350f / 12f);
+                hits[i - start].transform.localScale = new Vector3(1f * length + 0.01f, 0.05f, 0.05f);
+                hits[i - start].transform.Rotate(0, 0, angle, Space.Self);
+                hits[i - start].transform.localPosition = new Vector3(x + ((float)Math.Cos(angle)* (length + 0.01f) / 2), y + ((float)Math.Sin(angle) * (length + 0.01f) / 2), z);
+                */
+                //int side = Convert.ToInt32(Math.Floor(((float)Math.Atan(y / x) / 360f) * 12));
+                //Debug.Log(side + " " + Math.Atan(y / x));
+                
+                float xmag = 1;
+                if (x != 0) {
+                    xmag = (x / (float)Math.Abs(x));
+                }
+                float angle2 = (angle * (180 / (float)Math.PI) + 360) % 360;
+                int side = Convert.ToInt32(Math.Round(angle2 / (360f/12f)));
+                float desiredAngle = side * (360f / 12f);
+                //hits[i - start].transform.position = new Vector3(x + xmag*((float)Math.Cos(angle) * (length + 0.01f) / 2), y + ((float)Math.Sin(angle) * (length + 0.01f) / 2), z);
+                //hits[i - start].transform.RotateAround(pivot.transform.position, new Vector3(0, 0, 1), 0f * (180 / (float)Math.PI));
+
+                //hits[i - start].transform.position = new Vector3(x + ((float)Math.Cos(Math.PI * desiredAngle / 180.0) * (length + 0.01f) / 2), y + ((float)Math.Sin(Math.PI * desiredAngle / 180.0) * (length + 0.01f) / 2), z);
+
+                //hits[i - start].transform.position = new Vector3(0, 0, 0);
+
+                //Debug.Log(x + " " + desiredAngle+" "+Math.Cos(Math.PI * desiredAngle / 180.0) +" "+(x + ((float)Math.Cos(Math.PI*desiredAngle/180.0))));
+                hits[i - start].transform.RotateAround(pivot.transform.position, new Vector3(0, 0, 1), desiredAngle);
+                float magnitude = (float)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                float newX = xmag*(magnitude + ((length + 0.01f) / 2f)) * (float)Math.Cos(Math.PI * desiredAngle / 180.0f);
+                float newY = (magnitude + ((length + 0.01f) / 2f)) * (float)Math.Sin(Math.PI * desiredAngle / 180.0f);
+                hits[i - start].transform.position = new Vector3(newX, newY, z);
             }
         }
         
